72add7b776dfb966cecb245f27f6f7fbb5f7a8ba
nikol <nikolajmalafaev@gmail.com>
2022-01-18 18:53:01 +0300

    Добавлено следование камеры за игроком (MoveTowargas)

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Player player;
+    private Vector3 positionPlayer;
+    private Vector3 positionCamera;
+    private float speedPositionX;
+    private float thisSpeed;
+    private float positionX;
+    [SerializeField] private float MovidCameraSpeed;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        positionPlayer = player.transform.position;
+        positionX = positionPlayer.x;
+        positionX = Mathf.Clamp(positionX, -0.7f, 0.7f);
+
+        positionCamera = new Vector3(positionX, 0.54f, 1.2f);
+
+        transform.position = Vector3.MoveTowards(transform.position, positionCamera, MovidCameraSpeed * Time.deltaTime);
+        //if(player.m_CurrentLane > 0)
     }
 }
